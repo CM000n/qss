c36f1530f11b07caf1c302706c066a18ad0e66fb
Simon <simon.hoerrle@gmail.com>
2022-12-23 20:35:46 +0100

    put event handling to separate file

---
+++ custom_components/qss/event_handling.py
@@ -0,0 +1,33 @@
+"""Helper functions for event handling and data insertion."""
+from queue import Queue
+from typing import Callable
+
+from homeassistant.const import ATTR_ENTITY_ID, STATE_UNKNOWN
+from homeassistant.core import Event
+
+
+def put_event_to_queue(
+    event: Event, entity_filter: Callable[[str], bool], queue: Queue
+) -> None:
+    """Get events with new states and put them in the process queue."""
+    entity_id = event.data.get(ATTR_ENTITY_ID)
+    state = event.data.get("new_state")
+    if (
+        entity_id is not None
+        and state is not None
+        and state.state != STATE_UNKNOWN
+        and entity_filter(entity_id)
+    ):
+        queue.put(event)
+
+
+def get_event_from_queue(queue: Queue) -> Event:
+    """Return event from process queue."""
+    return queue.get()
+
+
+def finish_task_if_empty_event(event: Event, queue: Queue) -> None:
+    """Finish process queue task in case of no events."""
+    if event is None:
+        queue.task_done()
+        return
