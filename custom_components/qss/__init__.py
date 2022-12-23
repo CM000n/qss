70681f5befbb4de2f062f7ddce85381641e4be77
Simon <simon.hoerrle@gmail.com>
2022-12-23 11:54:41 +0100

    add db connection

--- custom_components/qss/__init__.py
+++ custom_components/qss/__init__.py
@@ -10,6 +10,7 @@ from typing import Any, Callable
 from questdb import ingress as qdb
 import voluptuous as vol
 
+from homeassistant.components import persistent_notification
 from homeassistant.const import (
     ATTR_ENTITY_ID,
     EVENT_HOMEASSISTANT_START,
@@ -99,6 +100,40 @@ class QuestDB(threading.Thread):
 
     def insert(self):
         """Initialize QSS and Insert data."""
+
+        tries = 1
+        connected = False
+
+        while not connected and tries <= 10:
+            if tries != 1:
+                time.sleep(CONNECT_RETRY_WAIT)
+            try:
+                self._setup_connection()
+                connected = True
+                _LOGGER.debug("Connected to ltss database")
+            except Exception as err:  # pylint: disable=broad-except
+                _LOGGER.error(
+                    "Error during connection setup: %s (retrying " "in %s seconds)",
+                    err,
+                    CONNECT_RETRY_WAIT,
+                )
+                tries += 1
+
+        if not connected:
+
+            @callback
+            def connection_failed():
+                """Connect failed tasks."""
+                self.async_db_ready.set_result(False)
+                persistent_notification.async_create(
+                    self.hass,
+                    "LTSS could not start, please check the log",
+                    "LTSS",
+                )
+
+            self.hass.add_job(connection_failed)
+            return
+
         shutdown_task = object()
         hass_started = concurrent.futures.Future()
 
@@ -150,6 +185,7 @@ class QuestDB(threading.Thread):
                     "Event Data is None: %s",
                     event,
                 )
+                self._close_connection()
                 self.queue.task_done()
                 return
 
@@ -212,3 +248,27 @@ class QuestDB(threading.Thread):
             and self.entity_filter(entity_id)
         ):
             self.queue.put(event)
+
+    def _setup_connection(self):
+        """Ensure database is ready to fly."""
+        try:
+            with qdb.Sender(self.host, self.port) as sender:
+                sender.connect()
+
+        except qdb.IngressError as err:
+            _LOGGER.error(
+                "Error during connecting to database: %s",
+                err,
+            )
+
+    def _close_connection(self):
+        """Close the connection."""
+        try:
+            with qdb.Sender(self.host, self.port) as sender:
+                sender.close()
+
+        except qdb.IngressError as err:
+            _LOGGER.error(
+                "Error during closing database connection: %s",
+                err,
+            )
