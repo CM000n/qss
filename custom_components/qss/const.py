9c4d2c225c08a7698b4cc95cc86a56c3d301197a
Simon <simon.hoerrle@gmail.com>
2023-02-13 10:21:41 +0100

    Update constants

--- custom_components/qss/const.py
+++ custom_components/qss/const.py
@@ -1,8 +1,24 @@
 """Constants for the QuestDB State Storage integration."""
 
+NAME = "QuestDB State Storage (QSS)"
 DOMAIN = "qss"
+ISSUES_URL = "https://github.com/CM000n/qss/issues"
+
 CONF_HOST = "host"
 CONF_PORT = "port"
 
+CONF_AUTH_KID = "auth_kid"
+CONF_AUTH_D_KEY = "auth_d_key"
+CONF_AUTH_X_KEY = "auth_x_key"
+CONF_AUTH_Y_KEY = "auth_y_key"
+
 RETRY_WAIT_SECONDS = 5
 RETRY_ATTEMPTS = 10
+
+STARTUP_MESSAGE = f"""
+-------------------------------------------------------------------
+Custom integration: {NAME}
+In case of any problem, please open a new issue here:
+{ISSUES_URL}
+-------------------------------------------------------------------
+"""
