d8e9a814c6940120bfc1df74f26486ad3bebc15a
Simon Hörrle <simon.hoerrle@dm.de>
2025-10-02 09:44:03 +0200

    Implement reuse of QuestDB sender

--- custom_components/qss/io.py
+++ custom_components/qss/io.py
@@ -5,7 +5,7 @@ from json import dumps
 from queue import Queue
 
 from homeassistant.core import Event
-from questdb.ingress import IngressError, Protocol, Sender
+from questdb.ingress import IngressError, Sender
 from tenacity import retry, retry_if_exception_type, stop_after_attempt, wait_fixed
 
 from .const import RETRY_ATTEMPTS, RETRY_WAIT_SECONDS
@@ -13,49 +13,22 @@ from .const import RETRY_ATTEMPTS, RETRY_WAIT_SECONDS
 _LOGGER = logging.getLogger(__name__)
 
 
-def _insert_row_with_auth(host: str, port: int, auth: tuple, event: Event) -> None:
-    with Sender(
-        Protocol.Tcps,
-        host,
-        port,
-        username=auth[0],
-        token=auth[1],
-        token_x=auth[2],
-        token_y=auth[3],
-        tls_verify=auth[4],
-    ) as sender:
-        entity_id = event.data["entity_id"]
-        state = event.data.get("new_state")
-        attrs = dict(state.attributes)
-        sender.row(
-            "qss",
-            symbols={"entity_id": entity_id},
-            columns={
-                "state": state.state,
-                "attributes": dumps(attrs, sort_keys=True, default=str),
-            },
-            at=event.time_fired,
-        )
+def _insert_row(sender: Sender, event: Event) -> None:
+    """Insert a single row using the provided sender."""
+    entity_id = event.data["entity_id"]
+    state = event.data.get("new_state")
+    attrs = dict(state.attributes)
+    sender.row(
+        "qss",
+        symbols={"entity_id": entity_id},
+        columns={
+            "state": state.state,
+            "attributes": dumps(attrs, sort_keys=True, default=str),
+        },
+        at=event.time_fired,
+    )
 
-        sender.flush()
-
-
-def _insert_row_without_auth(host: str, port: int, event: Event) -> None:
-    with Sender(Protocol.Tcp, host, port) as sender:
-        entity_id = event.data["entity_id"]
-        state = event.data.get("new_state")
-        attrs = dict(state.attributes)
-        sender.row(
-            "qss",
-            symbols={"entity_id": entity_id},
-            columns={
-                "state": state.state,
-                "attributes": dumps(attrs, sort_keys=True, default=str),
-            },
-            at=event.time_fired,
-        )
-
-        sender.flush()
+    sender.flush()
 
 
 @retry(
@@ -63,21 +36,18 @@ def _insert_row_without_auth(host: str, port: int, event: Event) -> None:
     wait=wait_fixed(RETRY_WAIT_SECONDS),
     retry=retry_if_exception_type(IngressError),
 )
-def _retry_data_insertion(host: str, port: int, auth: tuple, event: Event) -> None:
+def _retry_data_insertion(sender: Sender, event: Event) -> None:
     """Use a retry for inserting event data into QuestDB."""
-    if auth[0]:
-        _insert_row_with_auth(host, port, auth, event)
-    else:
-        _insert_row_without_auth(host, port, event)
+    _insert_row(sender, event)
 
 
 def insert_event_data_into_questdb(
-    host: str, port: int, auth: tuple, event: Event, queue: Queue
+    sender: Sender, event: Event, queue: Queue
 ) -> None:
-    """Insert given event data into QuestDB."""
+    """Insert given event data into QuestDB using reusable sender."""
     try:
-        _LOGGER.debug("%s %s %s %s", host, port, auth, event)
-        _retry_data_insertion(host, port, auth, event)
+        _LOGGER.debug("Inserting event: %s", event)
+        _retry_data_insertion(sender, event)
     except IngressError:
-        _LOGGER.exception()
+        _LOGGER.exception("Failed to insert event data into QuestDB")
     queue.task_done()
